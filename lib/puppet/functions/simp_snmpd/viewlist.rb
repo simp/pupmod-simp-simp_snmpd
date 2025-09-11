7e936959db36e1fd5445bb11c174d48fc29e9d4a
Steven Pritchard <steve@sicura.us>
2025-09-11 08:26:59 -0500

    Clean up for rubocop (#65)
    * Clean up for rubocop
    * Bump version
    * Update simp-rsync dependency

--- lib/puppet/functions/simp_snmpd/viewlist.rb
+++ lib/puppet/functions/simp_snmpd/viewlist.rb
@@ -4,7 +4,6 @@
 #    detail.
 
 Puppet::Functions.create_function(:'simp_snmpd::viewlist') do
-
   # @param view_hash
   #    The list of views to create.
   #
@@ -16,26 +15,25 @@ Puppet::Functions.create_function(:'simp_snmpd::viewlist') do
 
   def createlist(view_hash)
     viewlist = []
-    view_hash.each { | name, values|
-       viewpref = "#{name}"
-       if ! values.nil? and ! values.empty? then
-         values.each { |type, oids|
-           case "#{type}"
-           when /^(included|excluded)$/
-               if oids.is_a?(Array)
-                 oids.each { |elements|
-                   viewlist.push("#{viewpref} #{type} #{elements}")
-                 }
-               else
-                 elements = oids
-                 viewlist.push("#{viewpref} #{type} #{elements}")
-               end
-           else
-             fail("simp_snmpd: Badly formed view_hash entry #{name}.  Type key must be included or excluded and was #{type}.")
-           end
-         }
-       end
-    }
+    view_hash.each do |name, values|
+      viewpref = name.to_s
+      next unless !values.nil? && !values.empty?
+      values.each do |type, oids|
+        case type.to_s
+        when %r{^(included|excluded)$}
+          if oids.is_a?(Array)
+            oids.each do |elements|
+              viewlist.push("#{viewpref} #{type} #{elements}")
+            end
+          else
+            elements = oids
+            viewlist.push("#{viewpref} #{type} #{elements}")
+          end
+        else
+          raise("simp_snmpd: Badly formed view_hash entry #{name}.  Type key must be included or excluded and was #{type}.")
+        end
+      end
+    end
     viewlist
   end
 end
--- spec/functions/simp_snmpd/firewall_list_spec.rb
+++ spec/functions/simp_snmpd/firewall_list_spec.rb
@@ -2,25 +2,23 @@ require 'spec_helper'
 
 describe 'simp_snmpd::firewall_list' do
   context 'With valid params' do
-
     it 'returns an array' do
-      args = [ "udp:161",
-               "tcp:my.server.com",
-               "tcp6:961",
-               "udp6:[::1]:999",
-               "udp6:[::1f::gg]:777",
-               "dtlsudp:161",
-               "192.168.33.4:666"
-              ]
+      args = [ 'udp:161',
+               'tcp:my.server.com',
+               'tcp6:961',
+               'udp6:[::1]:999',
+               'udp6:[::1f::gg]:777',
+               'dtlsudp:161',
+               '192.168.33.4:666']
       retval = [
-               ['udp',161, 'ipv4'],
-               ['tcp',161,'ipv4'],
-               ['tcp',961,'ipv6'],
-               ['udp',777,'ipv6'],
-               ['udp',161, 'auto'],
-               ['udp',666,'ipv4'],
-             ]
-      is_expected.to run.with_params(args,).and_return(retval)
+        ['udp', 161, 'ipv4'],
+        ['tcp', 161, 'ipv4'],
+        ['tcp', 961, 'ipv6'],
+        ['udp', 777, 'ipv6'],
+        ['udp', 161, 'auto'],
+        ['udp', 666, 'ipv4'],
+      ]
+      is_expected.to run.with_params(args).and_return(retval)
     end
   end
 end
